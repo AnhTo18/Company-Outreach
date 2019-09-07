c3ea0cbce65a46a7a7b67aabdf48c24f57b4d931
Kordell Schrock <kschrock@iastate.edu>
2019-09-07 18:07:08 -0500

    Hello, World Class in Java

---
+++ Experiments/HelloWorld.java
@@ -0,0 +1,10 @@
+
+//The world says "Hello"
+//Random class for Hello World
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World!! I am Kordell. Ya-Yeet.");
+    }
+}
