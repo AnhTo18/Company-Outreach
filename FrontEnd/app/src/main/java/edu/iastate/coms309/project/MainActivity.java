2cf4266e270888474a2dba9171f9af57613fd16c
sjhenley <42706705+sjhenley@users.noreply.github.com>
2019-09-13 17:25:43 -0500

    begin work on QR reader

--- FrontEnd/app/src/main/java/edu/iastate/coms309/project/MainActivity.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project/MainActivity.java
@@ -1,19 +1,27 @@
-package com.example.testing;
+package edu.iastate.coms309.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b1;
-
         b1 = (Button) findViewById(R.id.button_qr);
+        b1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
 
+           }
+        });
     }
 }
