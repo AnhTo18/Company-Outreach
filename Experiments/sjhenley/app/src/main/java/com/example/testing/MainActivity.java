8855db6d3bbb19ee5a51efe773e8ae445d8895b4
Sam Henley <sjhenley@iastate.edu>
2019-09-17 16:37:20 -0500

    merge

--- Experiments/sjhenley/app/src/main/java/com/example/testing/MainActivity.java
+++ Experiments/sjhenley/app/src/main/java/com/example/testing/MainActivity.java
@@ -10,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button b1;
-
-        b1 = (Button) findViewById(R.id.button_qr);
-
     }
 }
