f2a16b2ebce710bdb9a4d6a9845d5101732db4c6
sjhenley <42706705+sjhenley@users.noreply.github.com>
2019-09-17 19:39:10 -0500

    added scanner activity, tried to initialize zxing scanner

--- FrontEnd/app/src/main/java/edu/iastate/coms309/project309/MainActivity.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project309/MainActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+// import android.support.v7.app.AppCompatActivity; cannot resolve symbol "v7"
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    public static TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.text_result);
+        b1 = findViewById(R.id.qr_button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Scanner.class));
+            }
+        });
     }
 }
