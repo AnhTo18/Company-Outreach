7001097ef7152c64a5589dbf04034f657af27123
Sam Henley <sjhenley@iastate.edu>
2019-09-18 12:13:46 -0500

    Added login activity, begun work on volley

--- FrontEnd/app/src/main/java/edu/iastate/coms309/project309/DevHome.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project309/DevHome.java
@@ -4,19 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.content.Intent;
 // import android.support.v7.app.AppCompatActivity; cannot resolve symbol "v7"
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+public class DevHome extends AppCompatActivity {
 
     Button b1;
     public static TextView result;
@@ -24,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dev_home);
 
         result = findViewById(R.id.text_result);
         b1 = findViewById(R.id.qr_button);
