b057f68ef1389817e49a2544f1375974c08b8e7e
Kordell Schrock <kschrock@iastate.edu>
2019-09-23 21:53:58 -0500

    Working on Server-Side of Project-Outreach

---
+++ BackEnd/project-outreach/src/main/java/myProject/App.java
@@ -0,0 +1,21 @@
+package myProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *inClass Spring Boot Application.
+ * Modified by Kordell
+ */
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	
+
+}
