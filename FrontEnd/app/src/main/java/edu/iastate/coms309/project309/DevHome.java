94fde2aa1b758b9760e1c25d2276bf9a79994f70
Sam Henley <sjhenley@iastate.edu>
2019-09-24 15:52:40 -0500

    Revert "Revert "pressing button now opens scanner camera""
    This reverts commit a98d50a831b98cfa7b71077ed439585e368a6bb5.

--- FrontEnd/app/src/main/java/edu/iastate/coms309/project309/DevHome.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project309/DevHome.java
@@ -26,8 +26,12 @@ public class DevHome extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),Scanner.class));
+                startScanner();
             }
         });
+    }
+
+    private void startScanner() {
+        new IntentIntegrator(this).initiateScan();
     }
 }
