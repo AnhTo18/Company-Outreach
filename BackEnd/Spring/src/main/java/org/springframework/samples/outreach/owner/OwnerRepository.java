60aaf399e0180f23301124db50e9f8b712e310d6
Kordell Schrock <kschrock@iastate.edu>
2019-09-30 00:25:08 -0500

    Added to Spring Backend

--- BackEnd/Spring/src/main/java/org/springframework/samples/outreach/owner/OwnerRepository.java
+++ BackEnd/Spring/src/main/java/org/springframework/samples/outreach/owner/OwnerRepository.java
@@ -29,4 +29,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OwnerRepository extends JpaRepository<Owners, Integer> {
 
+
 }
--- BackEnd/Spring/target/classes/db/mysql/schema.sql
+++ BackEnd/Spring/target/classes/db/mysql/schema.sql
@@ -7,3 +7,16 @@ CREATE TABLE IF NOT EXISTS owners (
   telephone VARCHAR(20),
   INDEX(last_name)
 ) engine=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS testing (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  address VARCHAR(255),
+  telephone VARCHAR(80),
+  points INT(5),
+  username VARCHAR(30),
+  password VARCHAR(30),
+  INDEX(last_name)
+) engine=InnoDB;
