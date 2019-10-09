da12d27d4e330698d46e1068d8633a2f752ee8ed
Kordell Schrock <kschrock@iastate.edu>
2019-10-08 22:46:01 -0500

    Added correct fields for registration. (i.e., username, points, and password.

---
+++ BackEnd/Spring/src/main/java/org/springframework/samples/outreach/owner/QrCodeController.java
@@ -0,0 +1,51 @@
+package org.springframework.samples.outreach.owner;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QrCodeController {
+
+	 @Autowired
+	    QrCodeRepository qrCodeRepository;
+	 
+	 private final Logger logger = LoggerFactory.getLogger(QrCodeController.class);
+	 
+	 
+	 /*get all qr codes*/
+	    @RequestMapping(method = RequestMethod.GET, path = "/qrcodes")
+	    public List<QrCode> getAllQrCodess() {
+	        logger.info("Entered into Controller Layer");
+	        List<QrCode> results = qrCodeRepository.findAll();
+	        logger.info("Number of Records Fetched:" + results.size());
+	        return results;
+	    }
+	    
+	    /*get qr code by id
+	     * This would be used for when they scan the QR code they can look it up for points
+	     * this in the future should return JSON format for points.
+	     * */
+	    @RequestMapping(method = RequestMethod.GET, path = "/qrcodes/{ownerId}")
+	    public Optional<QrCode> findQrCodeById(@PathVariable("qrcodeId") int id) {
+	        logger.info("Entered into Controller Layer");
+	        Optional<QrCode> results = qrCodeRepository.findById(id);
+	        return results;
+	    }
+	    
+	    //Admin use for creating QR Codes
+	    @RequestMapping(value= "/qrcodes/create", method= RequestMethod.POST)
+		public String createEmployee(@RequestBody QrCode code) {
+			System.out.println(this.getClass().getSimpleName() + " - Create new employee method is invoked.");
+			 qrCodeRepository.save(code);
+			 return "New QrCode by " + code.getCompany() + "Saved";
+		}
+}
---
+++ BackEnd/Spring/target/classes/db/mysql/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS owners (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  address VARCHAR(255),
+  city VARCHAR(80),
+  telephone VARCHAR(20),
+  INDEX(last_name)
+) engine=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS testing (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  address VARCHAR(255),
+  telephone VARCHAR(80),
+  points INT(5),
+  username VARCHAR(30),
+  password VARCHAR(30),
+  INDEX(last_name)
+) engine=InnoDB;
+
+INSERT INTO `testing` (`id`, `address`, `first_name`, `last_name`, `pass_word`, `points`, `telephone`, `user_name`) VALUES ('1', 'addy', 'Kordell', 'Schrock', 'password', '11', '555', 'my username');
+
+CREATE TABLE IF NOT EXISTS qrcode (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  points INT(4),
+  company VARCHAR(30),
+  INDEX(company)
+) engine=InnoDB;
+INSERT IGNORE INTO qrcode VALUES (NULL, 'Intel', '1000')
+INSERT IGNORE INTO qrcode VALUES (NULL, 'Ibm', '2500')
