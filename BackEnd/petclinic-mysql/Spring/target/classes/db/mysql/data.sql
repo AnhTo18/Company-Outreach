4fd639c9c054fc530dbebce2a83b82b596e9e5aa
not_creimers <creimers@iastate.edu>
2019-10-19 18:12:50 -0500

    cleaning up

--- BackEnd/petclinic-mysql/Spring/target/classes/db/mysql/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT IGNORE INTO owners VALUES (NULL, '110 W. Liberty St.', 'George', 'Franklin', '6085551023', 'ames','test', 'grapes','gfranklin@aol.com', 'gfranklin');
-INSERT IGNORE INTO owners VALUES (NULL, '638 Cardinal Ave.', 'Betty', 'Davis', '6085551749', 'des moines', 'crockpots23', 'chicken', 'bdavis@gmail,com', 'bdavis');
-INSERT IGNORE INTO owners VALUES (NULL, '2693 Commerce St.', 'Eduardo', 'Rodriquez', '6085558763','tama','say something', 'eggrolls', 'rodrigo23@aol.com', 'rdrigo');
-INSERT IGNORE INTO owners VALUES (NULL, '4510 Ontario St.', 'its', 'yaboi', '5155552555', 'weabland','itstaboispassword', 'falafel', 'idkr@idrk.com', 'gsg');
-
-INSERT IGNORE INTO qrcodes VALUES(NULL,'45454354','5424354354','35434354354354','56463468468','35435468748','55746574684674','64654684');
