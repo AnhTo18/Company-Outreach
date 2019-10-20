da75b8879a9de14d9cf9a95368b81f512b76b6a4
not_creimers <creimers@iastate.edu>
2019-10-19 22:41:53 -0500

    QR residue cleanup

--- BackEnd/Project-outreach/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.demo.entities.Product;
-
-@Repository("productRepository")
-public interface ProductRepository extends CrudRepository<Product, String> {
-}
