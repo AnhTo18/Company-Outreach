4ffac6d9b1ab14ffbfe9230d47dd17cf3e1f7ffe
not_creimers <creimers@iastate.edu>
2019-10-22 13:33:25 -0500

    added some methods and am trying to make them fully functional

--- BackEnd/Project-outreach/src/main/java/org/springframework/samples/outreach/qr/QRController.java
+++ BackEnd/Project-outreach/src/main/java/org/springframework/samples/outreach/qr/QRController.java
@@ -1,6 +1,10 @@
 package org.springframework.samples.outreach.qr;
 
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.samples.outreach.qr.ZXingHelper;
 import org.springframework.samples.outreach.owner.Owners;
 import org.springframework.samples.outreach.qr.ProductService;
@@ -24,6 +29,8 @@ public class QRController {
 
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
 	ProductRepository productRepo;
 	//Product p1;
 	  private final Logger logger = LoggerFactory.getLogger(QRController.class);
@@ -45,34 +52,117 @@ public class QRController {
 
 	
 	 //set quantity
-    @RequestMapping( method= RequestMethod.POST, value= "/qrcode/setqty/{id}")
-	public void SetQTY(@PathVariable int id, @RequestParam int quantity) throws Exception {
-    	System.out.print(this.getClass().getSimpleName() + "setting quantity of codes is invoked");
-    	Optional<Product> pr1 = productRepo.findById(id);
-    	Product p1 = new Product();
-    	if(!pr1.isPresent())
-    		throw new Exception("could not find qr with id: " + p1.getId());
-		else
-    	System.out.println(this.getClass().getSimpleName() + "setting qr quantity to: "
-    			+ quantity);
+	 @RequestMapping(value = "/{username}/{ password}/{ quant}", method = RequestMethod.GET)
+	    public Map<String, String> quant( @PathVariable("username") String company, @PathVariable(" password") String id, @PathVariable(" quant") String quant) {
+	    	
+		 company = company.toString().trim();
+        HashMap<String, String> map = new HashMap<>();
+        int currentId1 = 0;
+		 try {
+			 currentId1 = Integer.parseInt(id);
+		 }
+		 catch (NumberFormatException e)
+		 {
+			 currentId1 = 0; //not found
+		 }
+		 
+		 int currentquantity = 0;
+		 try {
+			 currentquantity = Integer.parseInt(quant);
+		 }
+		 catch (NumberFormatException e)
+		 {
+			 currentquantity = 0; //not found
+		 }
 		
-		p1.setQuantity(quantity);
-	}
+        Iterable<Product> yeet = productService.findAll(); //gets all the product types of qrcodes
+       
+		 for (Product current : yeet)
+		 {
+			 String currentCompany = current.getcompany().toString().trim();
+			 if(company.toString().trim().equals(currentCompany)) {
+				 
+				if(current.getId() == currentId1) {
+					 
+					 map.put("verify", "Added");
+
+	        		 current.setQuantity(currentquantity); //set current points to current user
+	        		
+	        		 productRepo.flush(); //update DataBase
+	        		 return map;
+	        		 
+				}
+
+			 }
+	           
+	    } 
+
+        map.put("verify", "false");
+        return map;
+    }
+	 @RequestMapping(value= "{id}/{company}", method= RequestMethod.POST)
+		public HashMap<String, String> findCode(@PathVariable("id") int id, @PathVariable("company") String company ) {
+		//This can be used once the user scans the code and gets the id and company. This will return json data of points to add to user
+		 //once the user gets back the info they can confirm then sends another post to the owner repo to update their points.
+		 company = company.toString().trim();
+	    	
+	    	Iterable<Product> results = productService.findAll();
+	        
+	        HashMap<String, String> map = new HashMap<>();
+	       
+	        
+	        for(Product current : results) {
+	        	String currentCompany = current.getcompany().toString().trim();
+	        	
+	        	if(company.toString().trim().equals(currentCompany))
+	        	{
+	        		
+	        		
+	        		if(current.getId() == id) {
+	        			
+	        			int change = current.getQuantity()-1;
+		        		current.setcompany(change +"");
+		        		
+		        		productRepo.flush(); //update repo
+		        		
+		        		if(current.getQuantity() <= 0) {
+		        			 map.put("points", "Error, Quantity is 0");
+		        			 //when there in not points left, Error message.
+		        			 return map;
+		        		}
+		        		
+	        		 int currentPoints;
+	        		 try {
+	        			 currentPoints = (int) current.getpoints();
+	        		 }
+	        		 catch (NumberFormatException e)
+	        		 {
+	        			 currentPoints = 0; //not found
+	        		 }
+	        		 
+	        		 map.put("points", currentPoints + "");
+	        		 return map;
+	        		}
+	        	
+	        	}
+	        }
+	         
+	    
+	        map.put("points", "NotFound");
+			 return map;
+		
+		}
 	
     //Consume a code
-    @RequestMapping( method= RequestMethod.POST, value= "/qrcode/use/{id}")
-	public void useCode(@PathVariable int id) throws Exception {
-    	System.out.print(this.getClass().getSimpleName() + "use a code is invoked");
-    	Optional<Product> pr1 = productRepo.findById(id);
-    	Product p1 = new Product();
-    	if(!pr1.isPresent())
-    		throw new Exception("could not find qr with id: " + p1.getId());
-    	else if(p1.getpoints()==0) 
-			System.out.print("there are no remaining codes");
-		else
-    	System.out.println(this.getClass().getSimpleName() + " 1 code from " + p1.getcompany() + "used");
-		
-		p1.setQuantity(p1.getQuantity()-1);
+    @RequestMapping( method= RequestMethod.POST, value= "/qrcode/use/{code}/{identifier}")
+    public void consume(@RequestParam("code") int code, @RequestParam("identifier") int identifier) throws Exception{
+    	
+    	String codeToString = Integer.toString(code);
+	    String identifierString = Integer.toString(identifier);
+	     
+	        HashMap<String, String> map = new HashMap<>();
+	        map.put(codeToString, identifierString);   
+	     
 	}
 
 }
