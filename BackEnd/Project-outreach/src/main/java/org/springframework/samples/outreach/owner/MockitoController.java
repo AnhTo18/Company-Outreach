7cc021ff5a6495c2869d4c4b1e95a7395cb943c8
not_creimers <creimers@iastate.edu>
2019-10-22 21:50:08 -0500

    updating pom

--- BackEnd/Project-outreach/src/main/java/org/springframework/samples/outreach/owner/MockitoController.java
+++ BackEnd/Project-outreach/src/main/java/org/springframework/samples/outreach/owner/MockitoController.java
@@ -31,7 +31,6 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 //import org.junit.Before;
 
