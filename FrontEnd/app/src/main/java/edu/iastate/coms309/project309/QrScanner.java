c97b9ef9a55907a3db817a0e82dbd261b53d27f2
Sam Henley <sjhenley@iastate.edu>
2019-10-25 15:02:49 -0500

    restoring frontend

--- FrontEnd/app/src/main/java/edu/iastate/coms309/project309/QrScanner.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project309/QrScanner.java
@@ -1,6 +1,10 @@
 package edu.iastate.coms309.project309;
 
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +19,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -27,89 +32,97 @@ import java.util.Map;
 import edu.iastate.coms309.project309.util.AppController;
 import edu.iastate.coms309.project309.util.Const;
 
-public class QrScanner extends AppCompatActivity implements View.OnClickListener {
+public class QrScanner extends AppCompatActivity {
     private Button scanQr;
     private TextView Resulttext;
-    private String qr;
-   // private String points="";
+
     private IntentIntegrator qrScan;
     RequestQueue rq;
-    JsonObjectRequest jor;
+    JsonObjectRequest jor, jor2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.qr_scanenr);
-       scanQr=(Button)findViewById(R.id.buttonQr);
-       Resulttext=(TextView)findViewById(R.id.Resulttext);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_scanner);
 
+        rq = Volley.newRequestQueue(this);
 
-       qrScan=new IntentIntegrator(this);
-       scanQr.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v){
-        qrScan.initiateScan();
-
-        JSONObject js = new JSONObject();
-        try {
-            js.put("qr", qr); //need to know where to send qr code
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        jor = new JsonObjectRequest(Request.Method.POST, Const.URL_QR, js, new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.d(AppController.TAG, response.toString());
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(AppController.TAG, "Error: " + error.getMessage());
-            }
-        }) {
-
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-
-
-                return params;
-            }
-        };
-
-        rq.add(jor);
+        new IntentIntegrator(this).initiateScan();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         IntentResult result= IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        /*
-        points="";
-        for(int i=0;i<result.toString().length();i++){
-            if(result.toString().substring(0,5).equals("309ss8")){
-                points=points+result.toString().substring(6,8);
-            }
-            else{
-
-            }
-        }
-        */
-
             if(result!=null){
-                if(result.getContents()==null){
-                    Toast.makeText(this,"No result found,",Toast.LENGTH_LONG).show();
-                }else{
-                    Resulttext.setText(result.toString());
-                    qr=result.toString();
+                if (result.getContents() == null) {
+
+                } else {
+
+                    Log.e(AppController.TAG, result.getContents());
+                    String url = Const.URL_QR + "/" + result.getContents();
+
+                    jor = new JsonObjectRequest(Request.Method.GET, result.getContents(), null, new Response.Listener<JSONObject>() {  //need qrURL
+                        String company, points;
+
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d(AppController.TAG, response.toString());
+
+
+                            try {
+                                points = response.getString("points");
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast t = Toast.makeText(getApplicationContext(), "JSON Error", Toast.LENGTH_SHORT);
+                                t.show();
+                            }
+
+                            String url2 = Const.URL_ADD_POINTS + "/" + points.substring(0, points.length() - 2) + "/" + Const.username;
+
+                            jor2 = new JsonObjectRequest(Request.Method.POST, url2, null, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Log.e(AppController.TAG, points + "Points added");
+
+                                    Toast t = Toast.makeText(getApplicationContext(), points + "points added!", Toast.LENGTH_SHORT);
+                                    t.show();
+
+                                    startActivity(new Intent(QrScanner.this, DevHomeActivity.class));
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast t = Toast.makeText(getApplicationContext(), "Volley Error", Toast.LENGTH_SHORT);
+                                    t.show();
+                                }
+                            });
+
+                            rq.add(jor2);
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            VolleyLog.d(AppController.TAG, "Error: " + error.getMessage());
+                            Toast t = Toast.makeText(getApplicationContext(), "Volley Error", Toast.LENGTH_SHORT);
+                            t.show();
+                        }
+                    });
+
+                    try {
+                        rq.add(jor);
+                    } catch (NullPointerException e) {
+                        Toast t = Toast.makeText(getApplicationContext(), "RQ Error", Toast.LENGTH_SHORT);
+                        t.show();
+                    }
+
                 }
-            }else{
+            } else{
                 super.onActivityResult(requestCode,resultCode,data);
             }
+
+
 
     }
 
--- FrontEnd/app/src/main/java/edu/iastate/coms309/project309/util/Const.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project309/util/Const.java
@@ -3,15 +3,11 @@ package edu.iastate.coms309.project309.util;
 public class Const {
     public static final String URL_REGISTER = "http://coms-309-ss-8.misc.iastate.edu:8080/owners/add";
     public static final String URL_SHOW_USERS = "http://coms-309-ss-8.misc.iastate.edu:8080/owners";
-<<<<<<< HEAD
-    public static final String URL_LOGIN = "http://coms-309-ss-8.misc.iastate.edu:8080/login";
-    public static final String URL_QR = "http://coms-309-ss-8.misc.iastate.edu:8080/product";
-    public static String current_user="";
-
-}
-
-=======
     public static final String URL_LOGIN = "http://coms-309-ss-8.misc.iastate.edu:8080/owners/login";
+    public static final String URL_QR = "http://coms-309-ss-8.misc.iastate.edu:8080/product/company/";
+    public static final String URL_ADD_POINTS = "http://coms-309-ss-8.misc.iastate.edu:8080/owners/addpoints";
+
+    public static String username = "";
 
 }
->>>>>>> kschrock
+
