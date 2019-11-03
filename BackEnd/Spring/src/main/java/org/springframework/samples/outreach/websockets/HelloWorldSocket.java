1b2850820cd8bb9b4c3b55599e81228d151a8191
not_creimers <creimers@iastate.edu>
2019-11-02 19:11:48 -0500

    websockets

---
+++ BackEnd/Spring/src/main/java/org/springframework/samples/outreach/websockets/HelloWorldSocket.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.outreach.websockets;
+
+import java.io.IOException;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+
+public class HelloWorldSocket {
+
+	
+	@ServerEndpoint(value = "/stream/{company}")
+	public class ChatEndpoint {
+	@OnOpen
+	public void onOpen(Session session) throws IOException {
+	// Get session and WebSocket connection
+	}
+	@OnMessage
+	public void onMessage(Session session, Message message) throws IOException {
+	// Handle new messages
+	}
+	@OnClose
+	public void onClose(Session session) throws IOException {
+	// WebSocket connection closes
+	}
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+	// Do error handling here
+	}
+	}
+	
+}
---
+++ BackEnd/Spring/src/main/java/org/springframework/samples/outreach/websockets/WebSocketConfig.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.outreach.websockets;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration 
+public class WebSocketConfig {  
+    @Bean  
+    public ServerEndpointExporter serverEndpointExporter(){  
+        return new ServerEndpointExporter();  
+    }  
+} 
