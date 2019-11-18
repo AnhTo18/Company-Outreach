1554cb931bb349e53eda1adffd701456bbdc0be7
Anh To <adto@iastate.edu>
2019-11-17 23:35:24 -0600

    minor fixes to paypal activity and xml

--- FrontEnd/app/src/main/java/edu/iastate/coms309/project309/PaymentDetails.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project309/PaymentDetails.java
@@ -3,46 +3,12 @@ package edu.iastate.coms309.project309;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.paypal.android.sdk.payments.PayPalConfiguration;
-import com.paypal.android.sdk.payments.PayPalPayment;
-import com.paypal.android.sdk.payments.PayPalPaymentDetails;
-import com.paypal.android.sdk.payments.PayPalService;
-import com.paypal.android.sdk.payments.PaymentActivity;
-import com.paypal.android.sdk.payments.PaymentConfirmation;
-
-import java.math.BigDecimal;
-
-import edu.iastate.coms309.project309.util.Const;
-import org.w3c.dom.Text;
 
 public class PaymentDetails extends AppCompatActivity {
 
@@ -50,7 +16,7 @@ public class PaymentDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment_detals);
+        setContentView(R.layout.activity_payment_details);
 
         txtId=(TextView)findViewById(R.id.txtId);
         txtAmount=(TextView)findViewById(R.id.txtAmount);
@@ -70,7 +36,7 @@ public class PaymentDetails extends AppCompatActivity {
         try {
             txtId.setText(response.getString("id"));
             txtStatus.setText((response.getString("state")));
-            txtAmount.setText((response.getString(String.format("$%s",paymentAmount))));
+            txtAmount.setText("$"+paymentAmount);
         }catch (JSONException e){
             e.printStackTrace();
         }
