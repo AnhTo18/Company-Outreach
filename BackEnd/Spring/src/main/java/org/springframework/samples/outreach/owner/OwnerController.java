85b06b9b2d55985da85cabe4ea7b987ea884beae
shsaimon <shsaimon@iastate.edu>
2019-11-18 07:03:17 +0000

    [SS] Update OwnerController.java to fix bug with subscriptions not being stored in db.

--- BackEnd/Spring/src/main/java/org/springframework/samples/outreach/owner/OwnerController.java
+++ BackEnd/Spring/src/main/java/org/springframework/samples/outreach/owner/OwnerController.java
@@ -81,8 +81,11 @@ class OwnerController {
 				 return map;
 			}
 			//its getting the owner by id and adding it to the list of owners in the company
-			company.getOwners().add(ownersRepository.findById(ownerID).get());
 		}
+		Owner owner = ownersRepository.findById(ownerID).get();
+		owner.getCompanies().add(company);
+		company.getOwners().add(owner);
+		ownersRepository.save(owner);
 		/*end subscription logic */
 		 map.put("verify", "Subscribed");
 		 return map;
