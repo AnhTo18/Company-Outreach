b02770f099d8aafbac7b7cfc5e90e24ae38b44e5
Sam Henley <sjhenley@iastate.edu>
2019-11-20 14:49:33 -0600

    optimized urls

--- FrontEnd/app/src/main/java/edu/iastate/coms309/project309/util/Const.java
+++ FrontEnd/app/src/main/java/edu/iastate/coms309/project309/util/Const.java
@@ -1,15 +1,16 @@
 package edu.iastate.coms309.project309.util;
 
 public class Const {
-    public static final String URL_REGISTER = "http://coms-309-ss-8.misc.iastate.edu:8080/owners/add";
-    public static final String URL_SHOW_USERS = "http://coms-309-ss-8.misc.iastate.edu:8080/owners/";
-    public static final String URL_LOGIN = "http://coms-309-ss-8.misc.iastate.edu:8080/owners/login";
-    public static final String URL_QR = "http://coms-309-ss-8.misc.iastate.edu:8080/product/company/";
-    public static final String URL_ADD_POINTS = "http://coms-309-ss-8.misc.iastate.edu:8080/owners/addpoints";
-    public static final String WS_EVENT_UPDATE = "ws://localhost:8080/notify/";
-    public static final String URL_EVENT_LIST = "http://coms-309-ss-8.misc.iastate.edu:8080/events/";
-    public static final String URL_SHOP = "http://coms-309-ss-8.misc.iastate.edu:8080/prize/";
-    public static final String URL_REDEEM = "http://coms-309-ss-8.misc.iastate.edu:8080/prize/redeem/";
+    public static final String DOMAIN =  "coms-309-ss-8.misc.iastate.edu:8080/";
+    public static final String URL_REGISTER = "http://" + DOMAIN + "owners/add";
+    public static final String URL_SHOW_USERS = "http://" + DOMAIN + "owners/";
+    public static final String URL_LOGIN = "http://" + DOMAIN +"owners/login";
+    public static final String URL_QR = "http://" + DOMAIN +"product/company/";
+    public static final String URL_ADD_POINTS = "http://" + DOMAIN +"owners/addpoints";
+    public static final String WS_EVENT_UPDATE = "ws://" + DOMAIN + "/notify/";
+    public static final String URL_EVENT_LIST = "http://" + DOMAIN + "events/";
+    public static final String URL_SHOP = "http://" + DOMAIN +"prize/";
+    public static final String URL_REDEEM =  "http://" + DOMAIN + "prize/redeem/";
 
     public static String username = "";
     public static String event = "";
